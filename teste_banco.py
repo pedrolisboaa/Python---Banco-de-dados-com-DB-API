847bcf5e5d25076e449de01d96ef8ca4b63eed9a
pedrolisboaa <phlisboa2000@gmail.com>
2022-09-14 19:55:39 -0300

    Primeiro commit, curso TREINA WEB

---
+++ teste_banco.py
@@ -0,0 +1,35 @@
+import MySQLdb
+
+banco = MySQLdb.connect(user="root", passwd="1234", db="clientes", host="localhost", port=3306, autocommit=False)
+cursor = banco.cursor()
+
+# CRUD - MOSTRANDO DADOS
+# Selecionado todos dados do banco
+# cursor.execute("SELECT * FROM cliente")
+# for cliente in cursor.fetchall():
+#     print(cliente)
+#
+# cursor.execute("SELECT * FROM cliente")
+# for cliente in cursor.fetchmany(2):
+#     print(cliente)
+
+
+# CRUD - INSERINDO DADO
+# cursor.execute("INSERT INTO cliente (nome, idade) VALUES ('Marcela', 24)")
+# # caso o autocommit la encima estava off é necessário ealizar o commit para subir para o banco banco.commit()
+# cursor.execute("SELECT * FROM cliente")
+# print(cursor.fetchall())
+
+# CRUD - ATUALIZAR DADO
+#cursor.execute("UPDATE cliente SET nome='PEDRO HENRIQUE' WHERE idade=29 and idcliente=1")
+
+# CRUD - DELETANDO DADO
+#cursor.execute("DELETE FROM cliente WHERE idcliente % 2 = 0")
+
+# Formar de inserir dado para evitar SQL INJECT
+
+
+
+
+banco.close()
+print('Banco fechado.')
